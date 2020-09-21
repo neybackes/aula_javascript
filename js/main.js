3cf81370b7aa2a5ed55e53d5cbf6d1338e014444
neybackes <backesney@gmail.com>
2020-09-21 17:44:55 -0300

    Arrays e Dicionairo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-var nome = "Ney Backes";
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]//exemṕlo lista dicionários
+console.log(frutas);
+alert(frutas[1].nome)
+
+/*var fruta = {nome:"maça", cor:"vermelha"} //exemplo dicionário
+console.log(fruta.nome);
+alert(fruta.cor); */
+
+
+
+
+//var lista = ["maça", "pêra", "laranaja"] //arrays
+//lista.push("uva"); //acrescentar um elemento
+//lista.pop(); //remover o último elemento
+//console.log(lista[1]);
+//alert(lista[1])
+//console.log(lista.length); //quantidade de elementos na lista
+//console.log(lista.reverse()); //ordem dos elementeos de trás para frente
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - ")); //separa elementos difernete da ,
+
+
+
+
+/*var nome = "Ney Backes";
 var n1 =  5;
 var n2 =  3;
 var frase =  "Japão é o melhor time do mundo"
@@ -8,4 +34,4 @@ var frase =  "Japão é o melhor time do mundo"
 console.log(nome);
 console.log(n1 * n2);
 console.log(frase.toLowerCase());
-//alert(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));*/
